1f9c2c39c5e8d6f2e7a40e81f0e74f7185eb0d29
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-03-27 01:32:32 -0300

    Front-end application

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -9,7 +9,7 @@ module.exports = {
     console.log(count);
 
     const incidents = await connection('incidents')
-      .join('ongs', 'ongs.id', '=', 'indicents.ong_id')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page -1) * 5)
       .select([
---
+++ frontend/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header({ children }) {
+  return (
+    <header>
+      <h1>{children}</h1>
+    </header>
+  );
+}
